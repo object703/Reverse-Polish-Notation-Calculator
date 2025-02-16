55131bd3f151e6d3fcb7d127d1fc1ee98f9e2073
object703 <24558755+object703@users.noreply.github.com>
2025-02-16 19:59:34 +0000

    Methods

---
+++ RPN/ArrayStack.cs
@@ -0,0 +1,28 @@
+﻿namespace RPN
+{
+    internal class ArrayStack<T> : IStack<T>
+    {
+        private T[] _array;
+        private int _head;
+
+        public bool IsEmpty()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Push(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
