8ca29ffb8458ec384341a146fbf6067007e84835
object703 <24558755+object703@users.noreply.github.com>
2025-02-17 16:13:19 +0000

    ArrayStack methods first implementation, not tested

--- RPN/ArrayStack.cs
+++ RPN/ArrayStack.cs
@@ -1,28 +1,71 @@
-﻿namespace RPN
+﻿using System;
+
+namespace RPN
 {
     internal class ArrayStack<T> : IStack<T>
     {
         private T[] _array;
-        private int _head;
+        private int _top;
+        private readonly int _length = 10;
 
-        public bool IsEmpty()
+        public ArrayStack()
         {
-            throw new System.NotImplementedException();
+            _array = new T[_length];
+            _top = -1;
         }
 
+        /// <summary>
+        /// Method for checking if stack is empty
+        /// </summary>
+        /// <returns>true, false</returns>
+        public bool IsEmpty() => _top == -1;
+        public bool IsFull() => _top == _length;
+
+        /// <summary>
+        /// Method to access the most recent element. No modifications
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new IndexOutOfRangeException("empty stack!");
+            }
+
+            return _array[_top];
         }
 
+        /// <summary>
+        /// Method to retrieve element
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new IndexOutOfRangeException("empty stack!");
+            }
+
+            return _array[_top--];
         }
 
+        /// <summary>
+        /// Method to add new item and increment _top
+        /// </summary>
+        /// <param name="item"></param>
         public void Push(T item)
         {
-            throw new System.NotImplementedException();
+            if (IsFull())
+            {
+                throw new IndexOutOfRangeException("stack is full!");
+            }
+            else
+            {
+                _top += 1;
+                _array[_top] = item;
+            }
         }
     }
 }
